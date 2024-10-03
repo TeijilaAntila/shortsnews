4de4d991aa74d12fca9678c4ba2fd3d0be2c01db
TeijilaAntila <phangfaunhuhu@gmail.com>
2024-10-03 12:46:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,14 +10,9 @@ fetch(url)
       const image = article.image_url ? `<img src="${article.image_url}" alt="${article.title}" />` : '';
       item.innerHTML = `
         <h2>${article.title}</h2>
+                ${image}
+
         <p>${article.content}</p>
-        <ul>
-          <li><strong>Author:</strong> ${article.author}</li>
-          <li><strong>Date:</strong> ${article.date}</li>
-          <li><strong>Time:</strong> ${article.time}</li>
-          <li><strong>URL:</strong> <a href="${article.url}" target="_blank">${article.url}</a></li>
-        </ul>
-        ${image}
       `;
       newsList.appendChild(item);
     });
